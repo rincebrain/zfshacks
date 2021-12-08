02331e3aa085c9031da9d158c355dad638b8c2c9
Rich Ercolani <rincebrain@gmail.com>
2021-12-08 04:40:43 -0500

    A couple more updates.
    Signed-off-by: Rich Ercolani <rincebrain@gmail.com>

--- tryzdbcolor.py
+++ tryzdbcolor.py
@@ -25,7 +25,9 @@ premap = {u"fletcher2":  88,
           u"ZFS plain file": 32,
           u"single": 1,
           u"double": 2,
-          u"gzip-\d": 123,}
+          u"gzip-\d": 123,
+          u"dedup": 16196,
+          }
 
 def cachehash(input):
     matched = False
@@ -45,10 +47,13 @@ def cachehash(input):
 def colorwrap(text,forcecolor=None):
 #  print("NUDEBUG: INPUT={}".format(text))
   if forcecolor is not None:
-    colorwith=str(int(forcecolor))
+    colorwith=str(int(forcecolor) % 16000)
   else:
-    colorwith=str(cachehash(text))
-  return str('\033[38;5;' + colorwith + 'm' + text + '\033[0m')
+    colorwith=str(cachehash(text) % 16000)
+  commandseq = '\033[38;5;' + colorwith + 'm'
+  if (cachehash(text) > 16000):
+    commandseq=commandseq + "\033[5;7m"
+  return str(commandseq + text + '\033[0m')
 
 #print(colorrange)
 for line in fileinput.input():
@@ -56,8 +61,8 @@ for line in fileinput.input():
       line = re.sub(r"(\W+)({})".format(predef),lambda s:'{}{}'.format(s.group(1),colorwrap(s.group(2))),line)
 #    lambda s: "DVA[%s]=<%s:%s:%s>".format(colorwrap(s.group(0),cachehash(s.group(0))),colorwrap(s.group(1),cachehash(s.group(1))),colorwrap(s.group(2),cachehash(s.group(2))),colorwrap(s.group(3),cachehash(s.group(3)))
     line = re.sub(r"DVA\[(\d)\]=<(\d):([0-9a-f]+):([0-9a-f]+)>",lambda s: "DVA[" + colorwrap(s.group(1)) +"]=<"+colorwrap(s.group(2))+":"+colorwrap(s.group(3))+":"+colorwrap(s.group(4))+">",line)
-    line = re.sub(r"(size|birth)=([0-9a-f]+)L/([0-9a-f]+)P",lambda s: "{}={}L/{}P".format(s.group(1),colorwrap(s.group(2)),colorwrap(s.group(3))),line)
-    line = re.sub(r"(size|birth)=([0-9a-f]+)L",lambda s: "{}={}L".format(s.group(1),colorwrap(s.group(2))),line)
+    line = re.sub(r"([0-9a-f]+)L/([0-9a-f]+)P",lambda s: "{}L/{}P".format(colorwrap(s.group(1)),colorwrap(s.group(2))),line)
+    line = re.sub(r"([0-9a-f]+)L",lambda s: "{}L".format(colorwrap(s.group(1))),line)
     line = re.sub(r"cksum=([0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+)",lambda s: "cksum={}".format(colorwrap(s.group(1),int("0x"+str(s.group(1).replace(":","")),16) % len(colorrange))),line)
     line = re.sub(r"\A(\W+)([0-9a-f]+)(\W)",lambda s: "{}{}{}".format(s.group(1),colorwrap(s.group(2)),s.group(3)),line)
     #line = re.sub(" fletcher4",' ' + colorwrap("fletcher4",cachehash("fletcher4")),line)
--- tryzfslistcolor.py
+++ tryzfslistcolor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import sys,fileinput,re,hashlib,functools
+import sys,fileinput,re,hashlib,functools,shutil,os
 from math import log,sqrt
 #[sys.stdout.write(re.sub("[A-Z][0-9]{2}[A-Z]",lambda s:hashlib.sha1(s.group(0)).hexdigest(),l))for l in fileinput.input()]
 
+
 cachelimit = 10000
 
-colorrange=list(range(1,6+1)) + list(range(9,15+1)) + list(range(17,230+1))
+colorrange=list(range(1,6+1)) + list(range(9,15+1)) + list(range(19,230+1))
 bytescale=[160,196,9,202,208,214,220,226,190,154,118,82,46,48,50,45,33,21,57,93,129,165,201]
 
 premap = {u"fletcher2":  88,
@@ -27,8 +28,10 @@ premap = {u"fletcher2":  88,
           u"ZFS plain file": 32,
           u"single": 1,
           u"double": 2,
-          u"gzip-\d": 123,}
+          u"gzip-\d": 123,
+          u"disabled": 160,}
 
+CLREOL=u"\x1B[K"
 
 @functools.lru_cache(maxsize=cachelimit)
 def cachehash(input):
@@ -52,33 +55,45 @@ def colorwrap(text,forcecolor=None):
     colorwith=str(int(forcecolor))
   else:
     colorwith=str(cachehash(text))
-  return str('\033[38;5;' + colorwith + 'm' + text + '\033[0m')
+  return str('\033[38;5;' + colorwith + 'm' + text + '\033[39m')
 
 def genericcolor(text):
   return colorwrap(text)
 
 def pathcolor(text):
   res = []
+  out = ""
   for s in re.split("/",text):
-    tmp = s.split("@")
-    out = colorwrap(tmp[0])
-    if len(tmp) == 2:
-      out += "@" + colorwrap(tmp[1])
+    for divider in ["@","#"]:
+      tmp = s.split(divider)
+      out = colorwrap(tmp[0])
+      if len(tmp) == 2:
+        out += divider + colorwrap(tmp[1])
+        break
     res.append(out)
   return "/".join(res)
 
 def ratiocolor(text):
-  bits = re.fullmatch(r"\A([0-9\.]+)(x)\Z",text)
+  bits = re.fullmatch(r"\A([0-9\.]+)(x?)\Z",text)
+  suf = ""
+  ret = ""
+  hashcol = 0
 #  print("\nDEBUG SIZECOL: {} {}".format(text,bits.groups()))
 #  hashcol = bytescale[int((float(bits.group(1)) / 1024.) * len(bytescale)-1)]
 #  hashcol = bytescale[int((log(float(bits.group(1))) / log(1024.)) * len(bytescale)-1)]
-  val = sqrt(float(bits.group(1)) - 1.0) / sqrt(4.0)
+  try:
+    val = sqrt(float(bits.group(1)) - 1.0) / sqrt(4.0)
 #  print("\nDEBUG SIZECOL VAL: {}".format(val))
-  if val >= 1.0:
-    val = 1.0
-  hashcol = bytescale[int(val * (len(bytescale)-1))]
+    if val >= 1.0:
+      val = 1.0
+    hashcol = bytescale[int(val * (len(bytescale)-1))]
 #  print("\nDEBUG SIZECOLOR: {} {} {} {} {}".format(text,hashcol,bits.group(1),bits.group(2),val))
-  return colorwrap(bits.group(1),forcecolor=hashcol) + colorwrap(bits.group(2))
+    if (bits.group(2) is not None):
+      suf = bits.group(2)
+    ret = colorwrap(bits.group(1),forcecolor=hashcol) + colorwrap(suf)
+  except:
+    ret = colorwrap(text)
+  return ret
 
 def sizecolor(text):
   bits = re.fullmatch(r"\A([0-9\.]+)([A-Z])?\Z",text)
@@ -87,7 +102,17 @@ def sizecolor(text):
 #  print("\nDEBUG SIZECOL: {} {}".format(text,bits.groups()))
 #  hashcol = bytescale[int((float(bits.group(1)) / 1024.) * len(bytescale)-1)]
 #  hashcol = bytescale[int((log(float(bits.group(1))) / log(1024.)) * len(bytescale)-1)]
-  hashcol = bytescale[int((sqrt(float(bits.group(1))) / sqrt(1024)) * len(bytescale)-1)]
+#  print("DBG int((sqrt(float(bits.group(1))) / sqrt(1024)) * len(bytescale)-1) = {}".format(int((sqrt(float(bits.group(1))) / sqrt(1024)) * len(bytescale)-1)))
+  # blasted -p
+#  if bits.group(2) is not None or int(bits.group(1)) <= 1024:
+#    hashcol = bytescale[int((sqrt(float(bits.group(1))) / sqrt(1024)) * len(bytescale)-1)]
+#  else:
+  val = float(bits.group(1))
+  while val > 1024.0:
+    val = val / 1000.0
+#  print("DBG: {} {} {}".format(text,bits.group(1),val))
+  hashcol = bytescale[int((sqrt(val) / sqrt(1024)) * len(bytescale)-1)]
+
   out = colorwrap(bits.group(1),forcecolor=hashcol)
   if len(bits.groups()) == 2 and bits.group(2) is not None:
     out += colorwrap(bits.group(2))
@@ -113,14 +138,29 @@ knowncols = {u"NAME": pathcolor,
              u"REFRATIO":ratiocolor,
             }
 
-for entry in bytescale:
-  print("{} ".format(colorwrap(str(entry),forcecolor=entry)),end="")
-print("")
+#for entry in bytescale:
+#  print("{} ".format(colorwrap(str(entry),forcecolor=entry)),end="")
+#print("")
 
+def linecolor(n=0):
+  ret = "\033[48;5;233m"
+  if (n % 2) == 0:
+    ret = "\033[49m"
+  return ret
+
+COLUMNS = shutil.get_terminal_size()[0]
+
+#DELIMITER=""
+#for i in range(COLUMNS):
+#  print("{} {}".format(str(i % 10),str(int(i / 10))))
+#  DELIMITER += colorwrap(str((i+1) % 10),forcecolor=cachehash(str(int(i / 10))))
+  
+linenum=0
 for line in fileinput.input():
+    charlength = 0
 #    print(line)
     if firstline:
-      if not re.fullmatch("\A[\sA-Z0-9_]+\Z",line):
+      if not re.fullmatch("\A[\sA-Z0-9_\-\:\.]+\Z",line):
         print(colorwrap("WARNING: column header not detected, running in generic mode...",forcecolor="8"))
         generichash = True
         globalgeneric = True
@@ -135,13 +175,19 @@ for line in fileinput.input():
 #          print("DEBUG COLUMN: {} {}".format(col,cols_inuse))
 #        continue
     cols = re.split("\s\s+",line)
-    whitesp = [""] + re.findall(r"(\s\s+)",line)
+    whitesp = [linecolor(linenum)] + re.findall(r"(\s\s+)",line)
     if not generichash and len(cols) != len(cols_inuse):
       print("Warning: splitter error, switching to generic hash for this line...")
       #cols = line.split()
       generichash = True
     dbgstr = ""
 #    print("DEBUG LINECNT: {} {} {} {}".format(len(cols),len(whitesp),cols,whitesp))
+    wht = 0
+    wholestr = ""
+    plainstr = ""
+    debugstr = ""
+    debugstr2 = ""
+    colmul=0
     for colN in range(len(cols)):
       if firstline:
         # FIXME: make it just print bold
@@ -151,9 +197,60 @@ for line in fileinput.input():
       else:
         colorfunc = genericcolor
 #      dbgstr += "DEBUG: A{}AB{}B".format(whitesp[colN],colorfunc(cols[colN]))
-      print("{}{}".format(whitesp[colN],colorfunc(cols[colN].strip())),end="")
+      wip = cols[colN].replace("\n","").expandtabs()
+      # we shoved the leading line color in here
+      if (colN == 0):
+        plainws = ""
+      else:
+        plainws = whitesp[colN].expandtabs()
+      ws_wip = whitesp[colN].expandtabs()
+#      print("W" + str(len(whitesp[colN])) + "X" + ws_wip + "Y" + str(len(ws_wip)) + "Z")
+#      print(ws_wip.encode("utf-8")[1:])
+#      print("X" + "{}".format(ws_wip) + "Y" + str(len("{}".format(ws_wip))) + "Z")
+      plainstr += plainws + wip
+      wholestr += "{}{}".format(ws_wip,colorfunc(wip))
+      wht += len(plainws + wip)
+      debugstr2 += " " + str(len(ws_wip)) + " " + str(len(wip))
+      if (len(plainws) > 0):
+        debugstr += (" " * (len(plainws)-1)) + "1"
+      if (len(wip) > 0):
+        debugstr += (" " * (len(wip)-1)) + "2"
+#      print("DEBUG: len(ln)={} len(wht)={} COLUMNS={}".format(len(ln),wht,COLUMNS))
+#      print(ln, end="")# + (" " * wht),end="")
+#    if (linenum > 1):
+#      print(CLREOL + "BEES")
+#    else:
+#    try:
+#      envc = os.environ["COLUMNS"]
+#    except:
+#      envc = -1
+#    print("\nDEBUG: wht={} COLUMNS={} env[COLUMNS]=={} len(COLUMNS-wht)={}\n".format(wht,COLUMNS,envc,len(" " * (COLUMNS - (wht % COLUMNS)))),end="")
+#    print(" " * (COLUMNS - (wht % COLUMNS)))
+    try:
+      MYC=os.environ["MYCOLUMNS"]
+    except:
+      MYC=-1
+    try:
+      COLS =os.environ["COLUMNS"]
+    except:
+      COLS=-1
+#    print(DELIMITER)
+    tmppad=""
+    while (colmul < len(plainstr)):
+      colmul += COLUMNS
+#    print("DEBUG: len(plainstr)={} len(wholestr)={} wht={} EVILCOLUMNS={} COLUMNS={} GETCOLUMNS={} MYC={} DIFF={}".format(len(plainstr),len(wholestr),wht,os.get_terminal_size(2)[0],COLS,shutil.get_terminal_size()[0],MYC,colmul-wht))
+#    print(debugstr2)
+#    print(debugstr,end="")
+    tmppad = " "*(colmul-wht)
+#    if len(tmppad) > 0:
+#      print(tmppad[:-1] + "3")
+#    print(plainstr)
+    print(wholestr,end="")
+    print(tmppad,end="")
+    print("\033[49m",end="")
     print("")
 #    print(dbgstr)
     if not globalgeneric:
       generichash = False
     firstline = False
+    linenum = (linenum+1) % 4
